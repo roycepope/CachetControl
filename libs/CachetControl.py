ee060d4f326e3adc14270b47f41c3faf300c4d9a
roycepope <roycepope@gmail.com>
2016-01-18 10:28:59 -0800

    Upload of requests libraries Added component statuses

--- libs/CachetControl.py
+++ libs/CachetControl.py
@@ -5,21 +5,28 @@ Created on Jan 17, 2016
 """
 
 import json
-import requests
+import libs.requests as requests
 
 
 class CachetControl(object):
     """
     classdocs
     """
+    __url = 'http://status.overlook.sh'
     __token = ''
     __header = {'X-Cachet-Token': __token}
+    __operational = 1
+    __issues = 2
+    __partial = 3
+    __major = 4
 
     '''
     Constructor
     '''
     def __init__(self):
-        self.__prefix = "http://status.overlook.sh/api/v1/"
+        self.__prefix = self.__url + "/api/v1/"
+        if not self.ping():
+            raise Exception("No Response from {0}".format(self.__url))
         pass
 
     '''
@@ -57,6 +64,18 @@ class CachetControl(object):
         json_data = json.loads(response.text)
         print json.dumps(json_data, indent=2)
         return
+
+    '''
+    Get json response for subscribers
+    '''
+    def getSubscribers(self):
+        try:
+            response = requests.get(self.__prefix + 'subscribers', headers=self.__header)
+        except requests.ConnectionError:
+            raise Exception("REST Server not found!")
+        json_data = json.loads(response.text)
+        print json.dumps(json_data, indent=2)
+        return
     '''
     Ping
     '''
@@ -65,9 +84,10 @@ class CachetControl(object):
             response = requests.get(self.__prefix + 'ping', headers=self.__header)
         except requests.ConnectionError:
             raise Exception("REST Server not found!")
-        json_data = json.loads(response.text)['data']
-        print json_data
-        return
+        if response.status_code == 200:
+            json_data = json.loads(response.text)['data']
+            return True
+        return False
 
     def mumbleMonitor(self):
         try:
