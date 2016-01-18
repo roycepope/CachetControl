1cf4227cdb157a93b0c123d2c4f9f9c50d0dd577
roycepope <roycepope@gmail.com>
2016-01-18 10:08:24 -0800

    Change filename, Initial upload

---
+++ website-monitor.py
@@ -0,0 +1,34 @@
+__author__ = 'Royce'
+
+import os
+from time import sleep
+from libs.CachetControl import CachetControl
+
+url = '/var/log/mumble-server.log'
+
+
+def tail(f, n):
+    stdin, stdout = os.popen2("tail -n " + str(n) + " " + f + "|grep Authenticated")
+    stdin.close()
+    lines = stdout.readlines()
+    stdout.close()
+    return lines
+
+monitoring = True
+users = []
+
+Cachet = CachetControl()
+while monitoring:
+    sleep(5)
+    mumble_tail = tail(mumble_log, 3)
+    print mumble_tail
+    if mumble_tail:
+        if users:
+            if users[-1] == mumble_tail[-1]:
+                print "Match!"
+        else:
+            users.append(mumble_tail[0])
+            print "Point Sent"
+            Cachet.mumbleMonitor()
+    else:
+        users = []
